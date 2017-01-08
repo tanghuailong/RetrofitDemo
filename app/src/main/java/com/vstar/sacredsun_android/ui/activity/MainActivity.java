c3fd620b6c5884c727454632015ca10dc18df79c
tanghuailong <tanghuailong@gmail.com>
2017-01-08 16:03:41 +0800

    feat: 界面初步设计

--- app/src/main/java/com/vstar/sacredsun_android/ui/activity/MainActivity.java
+++ app/src/main/java/com/vstar/sacredsun_android/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vstar.sacredsun_android;
+package com.vstar.sacredsun_android.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vstar.sacredsun_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
